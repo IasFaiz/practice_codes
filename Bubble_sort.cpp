1dec06d34cd3392ede9b0ed338d68dfd8e0c2b15
faiz <faiz@attentive.ai>
2024-08-15 15:53:01 +0530

    Bubble Sort

---
+++ Bubble_sort.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n;
+  cout << "Enter the number of elements: " << endl << endl;
+  cin >> n;
+  int arr[n];
+
+  cout << "Enter the number of elements: " << endl << endl;
+
+  return 0;
+}
